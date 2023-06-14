f07a8aed5971a13523cca5ba5cb93793cf0f456d
The-Higho <olatunjiisaacjolaoluwa@gmail.com>
2023-06-14 14:50:21 +0100

    updating with user input

--- src/Components/input.jsx
+++ src/Components/input.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Input = () => {
+const Input = ({ whenInputChange }) => {
     
   return (
-    // creating input field
-        <input type="text" className='theInput' placeholder='This is a user input'/>
+    // creating input field and adding the onchange attribute that calls the whenInputChange function to get the value
+        <input type="text" className='theInput ' placeholder='ENTER YOUR USERNAME' onChange={whenInputChange}/>
   )
 }
 
 export default Input
+
+
--- src/Components/output.jsx
+++ src/Components/output.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const Output = () => {
+const Output = (props) => {
   return (
     // Creating paragraphs 
-        <h1>
-            Osuporu Emmanuel is a bad client, he has been owing out law firm for years, and we are ready to take legal actions. Moving forward, we declare that we do not have any affiliation with the candidate
-                <br /><br />
-            Ogunmepon Sharafa, what a boy, very talented and skilled in the comedy industry. This year alone, he has bagged multiple awards including best skit maker of the year.
+        <h1 className='text-green-500'>
+           {props.username}
         </h1>
   )
 }
