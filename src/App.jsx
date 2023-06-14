5044b19e4c42cc4c7ca06b6574b312e528781efe
The-Higho <olatunjiisaacjolaoluwa@gmail.com>
2023-06-14 14:44:35 +0100

    Today's Assessment

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from './Components/input'
 import Output from './Components/output'
 
 const App = () => {
+  // Creating a username state that would be updated from the input component
+  const[username, setUsername] = useState('')
+
+  // Updating username with the value of the input 
+  const changeInput = (e) => {
+    setUsername(e.target.value);
+  };
   return (
-    <div className='Container'>
-        {/* Displaying multiple text from output.jsx component */}
-        <Output/>
-        <br />
-        <Output/>
-         {/* Displaying input field from input.jsx xomponent  */}
-        <Input/>
+    <div className='Container bg-black w-screen h-screen m- p-2'>
+        {/*Getting the username props from the username state */}
+        <Output username={username} />
+        {/*Adding */}
+        <Input whenInputChange={changeInput} /> 
     </div>
   )
 }
 
-export default App
+export default App 
